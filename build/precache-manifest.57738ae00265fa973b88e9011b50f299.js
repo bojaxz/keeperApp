07e504a36be8448a0f68e0f7a442dd02a6c6d324
Liam Morrison <liammorrison11@gmail.com>
2021-04-27 11:08:31 -0500

    Inital commit to keeperApp

---
+++ build/precache-manifest.57738ae00265fa973b88e9011b50f299.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0fbc35cc9095abb0ce769a4fa2342c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f5135dc64ffe0bfb31",
+    "url": "/static/js/2.9e2bc50f.chunk.js"
+  },
+  {
+    "revision": "f4123338e0e1518c981d",
+    "url": "/static/js/main.5179f41c.chunk.js"
+  },
+  {
+    "revision": "0b080249f0b73c0fa980",
+    "url": "/static/js/runtime-main.88f883b9.js"
+  }
+]);
